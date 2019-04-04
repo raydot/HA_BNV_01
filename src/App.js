812c8b842af159a1d350a5fabf6f0424d1a4e395
Dave Kanter <moatgoat@hotmail.com>
2019-04-04 14:30:03 -0700

    Added <BrowserRouter> to index.js

--- src/App.js
+++ src/App.js
@@ -2,28 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import {BrowserRouter as Router, Route, Link} from "react-router-dom"
+import {BrowserRouter as Router } from "react-router-dom"
 
 function App() {
     return (
 	<Router basename={'/test-app'}>
-	<div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-      </Router>
+  	<div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+    </Router>
     );
 }
 
