68ed48ab02cb779b64f1c6d1cf592dcea4f337d6
Dave Kanter <moatgoat@hotmail.com>
2019-04-09 16:39:03 -0700

    Added .htaccess to public folder.

---
+++ build/precache-manifest.7aac2696ba1e34cb7d4df02a993b9661.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "c2329a9f03602ba880e1",
+    "url": "./static/js/main.a77692d4.chunk.js"
+  },
+  {
+    "revision": "9e940c9cb5d6e23cc2d9",
+    "url": "./static/js/2.a8439355.chunk.js"
+  },
+  {
+    "revision": "c2329a9f03602ba880e1",
+    "url": "./static/css/main.bd9d3baf.chunk.css"
+  },
+  {
+    "revision": "6e3bde7a6066f59953fb01338e13e500",
+    "url": "./index.html"
+  }
+];
